f0e89cd809b972e4a2e936c051d9709b88c08953
Akbar Anung <akbaranung06@gmail.com>
2024-08-04 18:29:34 +0700

    update akbar

--- application/views/pages/auth/v_password.php
+++ application/views/pages/auth/v_password.php
@@ -174,3 +174,165 @@
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    // Format mata uang.
+    $('.uang').mask('000.000.000', {
+      reverse: true
+    });
+  })
+</script>
+<script>
+  $('#myDatepicker').datetimepicker();
+  $('#myDatepicker2').datetimepicker({
+    format: 'YYYY-MM-DD'
+  });
+  $('#myDatepicker3').datetimepicker({
+    format: 'hh:mm A'
+  });
+  $('#myDatepicker4').datetimepicker({
+    ignoreReadonly: true,
+    allowInputToggle: true
+  });
+  $('#datetimepicker6').datetimepicker();
+  $('#datetimepicker7').datetimepicker({
+    useCurrent: false
+  });
+  $("#datetimepicker6").on("dp.change", function(e) {
+    $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+  });
+  $("#datetimepicker7").on("dp.change", function(e) {
+    $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+  });
+  //currency
+  $("input[data-type='currency']").on({
+    keyup: function() {
+      formatCurrency($(this));
+    },
+    blur: function() {
+      formatCurrency($(this), "blur");
+    }
+  });
+
+  function clicked() {
+    return confirm('Are you sure to save new Customer?');
+  }
+
+  function reset_form() {
+    document.getElementById("form_input").reset();
+  }
+
+  function formatNumber(n) {
+    // format number 1000000 to 1,234,567
+    return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  }
+
+  function formatCurrency(input, blur) {
+    // appends $ to value, validates decimal side
+    // and puts cursor back in right position.
+    // get input value
+    var input_val = input.val();
+    // don't validate empty input
+    if (input_val === "") {
+      return;
+    }
+    // original length
+    var original_len = input_val.length;
+    // initial caret position 
+    var caret_pos = input.prop("selectionStart");
+    // check for decimal
+    if (input_val.indexOf(".") >= 0) {
+      // get position of first decimal
+      // this prevents multiple decimals from
+      // being entered
+      var decimal_pos = input_val.indexOf(".");
+      // split number by decimal point
+      var left_side = input_val.substring(0, decimal_pos);
+      var right_side = input_val.substring(decimal_pos);
+      // add commas to left side of number
+      left_side = formatNumber(left_side);
+      // validate right side
+      right_side = formatNumber(right_side);
+      // On blur make sure 2 numbers after decimal
+      if (blur === "blur") {
+        right_side += "00";
+      }
+      // Limit decimal to only 2 digits
+      right_side = right_side.substring(0, 2);
+      // join number by .
+      input_val = "Rp " + left_side + "." + right_side;
+    } else {
+      // no decimal entered
+      // add commas to number
+      // remove all non-digits
+      input_val = formatNumber(input_val);
+      input_val = "Rp " + input_val;
+      // final formatting
+      if (blur === "blur") {
+        input_val += ".00";
+      }
+    }
+    // send updated string to input
+    input.val(input_val);
+    // put caret back in the right position
+    var updated_len = input_val.length;
+    caret_pos = updated_len - original_len + caret_pos;
+    input[0].setSelectionRange(caret_pos, caret_pos);
+  }
+</script>
+<script>
+  $(document).ready(function() {
+    $(document).on('change', '.btn-file :file', function() {
+      var input = $(this),
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+      input.trigger('fileselect', [label]);
+    });
+
+    $('.btn-file :file').on('fileselect', function(event, label) {
+
+      var input = $(this).parents('.input-group').find(':text'),
+        log = label;
+
+      if (input.length) {
+        input.val(log);
+      } else {
+        if (log) alert(log);
+      }
+
+    });
+
+    function readURL(input) {
+      if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+          $('#img-upload').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+      }
+    }
+
+    $("#imgInp").change(function() {
+      readURL(this);
+    });
+  });
+
+  //sweet alert
+  $('#btn-submit').on('click', function(e) {
+    e.preventDefault();
+    var form = $(this).parents('form');
+    swal({
+      title: "Are you sure?",
+      text: "Password wil be save!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, save it!",
+      closeOnConfirm: false
+    }, function(isConfirm) {
+      if (isConfirm) form.submit();
+    });
+  });
+</script>
