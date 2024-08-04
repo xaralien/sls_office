d3d5db4c9cd465fe96b711c33683383d0729f4b9
Akbar Anung <akbaranung06@gmail.com>
2024-08-04 17:11:41 +0700

    update akbar

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,56 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'kodesisi_sls',
+// 	'password' => 'sls123!@#',
+// 	'database' => 'kodesisi_sls_core',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
+// $db['corebank'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'username' => 'kodesisi_sls',
+// 	'password' => 'sls123!@#',
+// 	'database' => 'kodesisi_sls_corebank',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'kodesisi_sls',
-	'password' => 'sls123!@#',
-	'database' => 'kodesisi_sls_core',
+	'hostname' => 'localhost:3307',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'bdl_core',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -97,10 +141,10 @@ $db['default'] = array(
 
 $db['corebank'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'kodesisi_sls',
-	'password' => 'sls123!@#',
-	'database' => 'kodesisi_sls_corebank',
+	'hostname' => 'localhost:3307',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'corebank',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
