9028b98f4b95bc1d44625c3d8d6e06a6d03bcf6d
xaralien <dimasuzumaki126@gmail.com>
2024-11-12 13:52:10 +0700

    update

--- application/views/pages/detail_no_lambung/detail_list.php
+++ application/views/pages/detail_no_lambung/detail_list.php
@@ -2,6 +2,7 @@
 <div class="right_col" role="main">
     <div class="clearfix"></div>
     <!-- Start content-->
+    <!-- tes -->
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel card">
